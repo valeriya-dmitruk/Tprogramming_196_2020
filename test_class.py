33ebb10b254e55716e90ad671b78aa0b37852a82
Valeriya <lera_dmitruk777@mail.ru>
2020-04-11 11:18:22 +0300

    test k lab2

--- test_class.py
+++ test_class.py
@@ -1,8 +1,31 @@
 import unittest
+import Dog
 
 class ClassTests(unittest.TestCase):
     def test_empty(self):
         self.assertTrue(True)
+
+    def test_creation(self):
+        tst = Dog.Dog("Dog")  
+        self.assertEqual("Dog", tst.name)
+
+    def test_age_change(self):
+        tst = Dog.Dog("Dog")
+        self.assertEqual("Dog", tst.name)
+        tst.age = 5
+        self.assertEqual(5, tst.age)
+
+    def test_wrong_age_change(self):
+        tst = Dog.Dog("Dog") 
+        self.assertEqual("Dog", tst.name) 
+        tst.age = -5 
+        self.assertEqual(0, tst.age) 
+
+    def test_wrong_type_age_change(self):
+        tst = Dog.Dog("Dog")  
+        self.assertEqual("Dog", tst.name) 
+        tst.age = "5 year"
+        self.assertEqual(0, tst.age)  
 
 if __name__ == '__main__':
     unittest.main()
